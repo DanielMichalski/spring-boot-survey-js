fad598c4b5e74200b646ad4bfa22606a1a6be88e
Daniel Michalski <michalskidaniel2@gmail.com>
2023-11-21 19:55:26 +0100

    Setup static code analysis tools and reformat the code

---
+++ src/main/java/com/danielmichalski/surveyjs/SurveyJSApplication.java
@@ -0,0 +1,13 @@
+package com.danielmichalski.surveyjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyJSApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SurveyJSApplication.class, args);
+  }
+
+}
